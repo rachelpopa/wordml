b14db10bcfade0970060bd9084f15a18b24c8f28
Torchid <rachel.l.popa@gmail.com>
2023-10-07 21:24:00 -0600

    Mess with styling more

--- lib/view/Keyboard.ml
+++ lib/view/Keyboard.ml
@@ -1,6 +1,10 @@
-let keyboard_key letter =
+let keyboard_key_small letter =
   let open Tyxml.Html in
-  button ~a:[ a_class [ "keyboard-key" ] ] [ txt letter ]
+  button ~a:[ a_class [ "keyboard-key small" ] ] [ txt letter ]
+
+let keyboard_key_large letter =
+  let open Tyxml.Html in
+  button ~a:[ a_class [ "keyboard-key large" ] ] [ txt letter ]
 
 let keyboard_row keys =
   let open Tyxml.Html in
@@ -10,37 +14,37 @@ let keyboard =
   let open Tyxml.Html in 
   div ~a:[ a_class [ "keyboard" ] ]
   [ keyboard_row 
-    [ keyboard_key "Q"
-    ; keyboard_key "W"
-    ; keyboard_key "E"
-    ; keyboard_key "R"
-    ; keyboard_key "T"
-    ; keyboard_key "Y"
-    ; keyboard_key "U"
-    ; keyboard_key "I"
-    ; keyboard_key "O"
-    ; keyboard_key "P"
+    [ keyboard_key_small "Q"
+    ; keyboard_key_small "W"
+    ; keyboard_key_small "E"
+    ; keyboard_key_small "R"
+    ; keyboard_key_small "T"
+    ; keyboard_key_small "Y"
+    ; keyboard_key_small "U"
+    ; keyboard_key_small "I"
+    ; keyboard_key_small "O"
+    ; keyboard_key_small "P"
     ]
   ; keyboard_row
-    [ keyboard_key "A"
-    ; keyboard_key "S"
-    ; keyboard_key "D"
-    ; keyboard_key "F"
-    ; keyboard_key "G"
-    ; keyboard_key "H"
-    ; keyboard_key "J"
-    ; keyboard_key "K"
-    ; keyboard_key "L"
+    [ keyboard_key_small "A"
+    ; keyboard_key_small "S"
+    ; keyboard_key_small "D"
+    ; keyboard_key_small "F"
+    ; keyboard_key_small "G"
+    ; keyboard_key_small "H"
+    ; keyboard_key_small "J"
+    ; keyboard_key_small "K"
+    ; keyboard_key_small "L"
     ]
   ; keyboard_row
-    [ keyboard_key "ENTER"
-    ; keyboard_key "Z"
-    ; keyboard_key "X"
-    ; keyboard_key "C"
-    ; keyboard_key "V"
-    ; keyboard_key "B"
-    ; keyboard_key "N"
-    ; keyboard_key "M"
-    ; keyboard_key "DELETE"
+    [ keyboard_key_large "ENTER"
+    ; keyboard_key_small "Z"
+    ; keyboard_key_small "X"
+    ; keyboard_key_small "C"
+    ; keyboard_key_small "V"
+    ; keyboard_key_small "B"
+    ; keyboard_key_small "N"
+    ; keyboard_key_small "M"
+    ; keyboard_key_large "DELETE"
     ]
   ]
