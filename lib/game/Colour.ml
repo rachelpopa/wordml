402e30bf3c7176c5b93226ea9275e15c4acce828
Torchid <rachel.l.popa@gmail.com>
2023-10-16 21:37:27 -0600

    fix colours

--- lib/game/Colour.ml
+++ lib/game/Colour.ml
@@ -9,13 +9,15 @@ let get_colour letter_guess position =
                 | _ -> aux (index + 1) t "yellow"
               end
               else aux (index + 1) t colour
-  in aux 1 (Util.String.explode_string Guess.solution) "grey"
+  in aux 0 (Util.String.explode_string Guess.solution) "grey"
 
 let get_key_colour key guesses =
-  let rec aux guess_string = 
-    match guess_string with
-    | [] -> "light-grey"
-    | h::t -> if h = key then (get_colour key ((List.length (h::t)) mod 5))
-              else aux t
-  in aux (Util.String.explode_string (String.concat "" guesses))
-  
+  let rec aux guess_string colour = 
+    if colour = "green" then colour
+    else
+      match guess_string with
+      | [] -> colour
+      | h::t -> if h = key then let () = print_endline((Char.escaped key) ^ string_of_int ((List.length (guess_string)))) in
+                    aux t (get_colour key (((List.length guess_string) - 1) mod 5))
+                else aux t colour
+  in aux (List.rev(Util.String.explode_string (String.concat "" guesses))) "light-grey"
